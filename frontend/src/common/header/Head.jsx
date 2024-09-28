abc317c7b4b746798a87b83696a4584592768bd0
IT21063664 <it21063664@my.sliit.lk>
2024-09-28 22:28:46 +0530

    Dashbord

---
+++ frontend/src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <div>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +94774789444</label>
+            <i className='fa fa-envelope'></i>
+            <label> support@ui-lib.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Head
---
+++ frontend/src/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Header.css"
+import Head from './Head'
+import Search from './Search'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <>
+        <Head/>
+        <Search/>
+        <Navbar/>
+    </>
+  )
+}
+
+export default Header
